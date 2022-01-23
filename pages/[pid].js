d0b6ca8c7647d1ebded5c106ed4b73fb16fe4cba
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-23 21:15:12 +0700

    loading paths dynamically

--- pages/[pid].js
+++ pages/[pid].js
@@ -8,7 +8,7 @@ function Product(props) {
   // if (!loadedProduct) {
   //   return <p>Loading...</p>
   // }
-  
+
   return (
     <Fragment>
       <h2>{loadedProduct.title}</h2>
@@ -17,12 +17,18 @@ function Product(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
   return {
@@ -33,11 +39,13 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const paramsWithIds = data.products.map((product) => ({
+    params: { pid: product.id },
+  }));
   return {
-    paths: [
-      { params: { pid: "p1" } },
-    ],
-    fallback: 'blocking'
-  }
+    paths: paramsWithIds,
+    fallback: false,
+  };
 }
 export default Product;
